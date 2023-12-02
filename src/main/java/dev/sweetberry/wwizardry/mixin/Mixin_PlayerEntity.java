f8fea353095fd92a84f98f863a3e83b82d8b0f48
Octal <oliver@olivermakesco.de>
2023-12-01 20:35:31 -0600

    Make Mixin_PlayerEntity less priority, clean up build.gradle

--- src/main/java/dev/sweetberry/wwizardry/mixin/Mixin_PlayerEntity.java
+++ src/main/java/dev/sweetberry/wwizardry/mixin/Mixin_PlayerEntity.java
@@ -5,13 +5,18 @@ import dev.sweetberry.wwizardry.Badges;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import org.quiltmc.loader.api.QuiltLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PlayerEntity.class)
+@Mixin(
+	value = PlayerEntity.class,
+	priority = 2000
+)
 public abstract class Mixin_PlayerEntity {
 	@Shadow
 	public abstract GameProfile getGameProfile();
@@ -21,6 +26,9 @@ public abstract class Mixin_PlayerEntity {
 		at = @At("RETURN")
 	)
 	private void wwizardry$getBadge(CallbackInfoReturnable<Text> cir) {
+		// TODO: don't hardcode
+		if (QuiltLoader.isModLoaded("styled-nicknames") || QuiltLoader.isModLoaded("styledchat"))
+			return;
 		if (!(cir.getReturnValue() instanceof MutableText mutableText))
 			return;
 		var badge = Badges.getBadgeFor(getGameProfile().getId());
