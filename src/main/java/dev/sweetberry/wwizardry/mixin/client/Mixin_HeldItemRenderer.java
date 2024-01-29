174e3363745e8b67716097df0d9fac7487762d5d
Oliver-makes-code <oliver@olivermakesco.de>
2024-01-29 17:36:37 -0600

    Invert if in Mixin_HeldItemRenderer to make control flow more understandable

--- src/main/java/dev/sweetberry/wwizardry/mixin/client/Mixin_HeldItemRenderer.java
+++ src/main/java/dev/sweetberry/wwizardry/mixin/client/Mixin_HeldItemRenderer.java
@@ -25,48 +25,55 @@ public abstract class Mixin_HeldItemRenderer {
 		)
 	)
 	private void wwizardry$renderFirstPersonItem(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
-		if (item.getItem() instanceof SoulMirrorItem && player.isUsingItem() && player.getItemUseTimeLeft() > 0 && player.getActiveHand() == hand) {
-			if (ModClient.useItemTick == -1)
-				ModClient.useItemTick = ModClient.tickCounter;
-			Arm arm = (hand == Hand.MAIN_HAND) ? player.getMainArm() : player.getMainArm().getOpposite();
-
-			int i = arm == Arm.RIGHT ? 1 : -1;
-
-			matrices.translate(
-				(float)i * 0.41f,
-				-0.41f, //y
-				-0.7f
-			);
-
-			int timeSinceUse = ModClient.tickCounter - ModClient.useItemTick;
-
-			float time = (float)timeSinceUse + tickDelta;
-
-			final int chargeTime = 30;
-			final int wobbleStart = 15;
-			final float addedWidth = 0.25f;
-			final float finalWidth = 1+addedWidth;
-			final float wobbleRate = 0.4f;
-			final float wobbleDepth = 0.02f;
-
-			float stretch;
-
-			if (time < chargeTime)
-				stretch = ((time/chargeTime)*addedWidth)+1;
-			else
-				stretch = finalWidth;
-
-			float wobble = 0;
-			float wobbleTime = time-chargeTime;
-
-			if (time > wobbleStart)
-				wobble = (float)Math.sin(wobbleTime*wobbleRate)*wobbleDepth;
-
-			matrices.translate(wobble, 0, 0);
-
-			matrices.scale(1, 1, stretch);
-		} else {
+		if (!(
+			item.getItem() instanceof SoulMirrorItem
+			&& player.isUsingItem()
+			&& player.getItemUseTimeLeft() > 0
+			&& player.getActiveHand() == hand)
+		) {
 			ModClient.useItemTick = -1;
+			return;
 		}
+
+		if (ModClient.useItemTick == -1)
+			ModClient.useItemTick = ModClient.tickCounter;
+
+		Arm arm = (hand == Hand.MAIN_HAND) ? player.getMainArm() : player.getMainArm().getOpposite();
+
+		int i = arm == Arm.RIGHT ? 1 : -1;
+
+		matrices.translate(
+			(float)i * 0.41f,
+			-0.41f, //y
+			-0.7f
+		);
+
+		int timeSinceUse = ModClient.tickCounter - ModClient.useItemTick;
+
+		float time = (float)timeSinceUse + tickDelta;
+
+		final int chargeTime = 30;
+		final int wobbleStart = 15;
+		final float addedWidth = 0.25f;
+		final float finalWidth = 1+addedWidth;
+		final float wobbleRate = 0.4f;
+		final float wobbleDepth = 0.02f;
+
+		float stretch;
+
+		if (time < chargeTime)
+			stretch = ((time/chargeTime)*addedWidth)+1;
+		else
+			stretch = finalWidth;
+
+		float wobble = 0;
+		float wobbleTime = time-chargeTime;
+
+		if (time > wobbleStart)
+			wobble = (float)Math.sin(wobbleTime*wobbleRate)*wobbleDepth;
+
+		matrices.translate(wobble, 0, 0);
+
+		matrices.scale(1, 1, stretch);
 	}
 }
